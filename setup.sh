30281ce0bea2b5ede1c5ba2b4c4fd7aa649dfe4a
Siddarth Reddy <siddarthreddy4923@gmail.com>
2021-07-08 22:35:51 +0530

    added installation script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+sudo apt update && sudo apt upgrade
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install nodejs npm surf
+sudo apt install build-essential
+npm install wetty@2.0.2
+
+autoload -U colors
+colors
+printf "$bold_color$fg[yellow]IF YOU DID NOT GET ANY ERRORS TILL NOW THEN YOU CAN GO AHEAD AND RUN ./launch.sh\n"
+echo "$bold_color$fg[red] YOU CAN IGNORE THE WARNINGS THEY ARE QUITE NORMALIN THIS INSTALLATINS"
+
+
